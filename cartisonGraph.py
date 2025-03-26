9ff45e96954b8ec5298e64c5ef4c6c4a9f14f9e5
NightShiftKing <michaelmonreal482@gmail.com>
2025-03-25 20:46:04 -0600

    made alot of changes

---
+++ cartisonGraph.py
@@ -0,0 +1,61 @@
+import pygame
+
+class CartisonCords():
+    def __init__(self ):
+
+        
+        WIDTH , HEIGHT = pygame.display.get_surface().get_size()
+        TOP_MIDDLE = (WIDTH/2, 0)
+        BOTTOM_MIDDLE = (WIDTH/2, HEIGHT)
+
+        MIDDLE_LEFT = (0, HEIGHT/2)
+        MIDDLE_RIGHT = (WIDTH, HEIGHT/2)
+
+
+
+        self.X_Axis = (MIDDLE_LEFT, MIDDLE_RIGHT)
+        self.Y_Axis = (TOP_MIDDLE,BOTTOM_MIDDLE)
+        self.Increments = 10
+
+        self.BLACK = (0,0,0)
+
+
+
+
+
+
+
+    def drawCartisonCords(self, screen):
+        pygame.draw.line(screen, self.BLACK, self.X_Axis[0], self.X_Axis[1])
+        pygame.draw.line(screen, self.BLACK, self.Y_Axis[0], self.Y_Axis[1])
+
+        #draws the tick marks for x axis
+        try:
+            for i in range(self.X_Axis[0][0], self.X_Axis[1][0], self.Increments):
+                
+                pygame.draw.line(screen, self.BLACK, (i, self.X_Axis[0][1]+5), (i, self.X_Axis[0][1]-5))  
+                pygame.draw.line(screen, self.BLACK, (self.Y_Axis[0][0]+5,i ), (self.Y_Axis[0][0]-5,i )) 
+        except ValueError as e:
+            if str(e) != "range() arg 3 must not be zero": 
+                raise e 
+            else:
+                self.Increments = 1 
+
+
+              
+
+    def update(self):
+        WIDTH , HEIGHT = pygame.display.get_surface().get_size()
+        TOP_MIDDLE = (WIDTH/2, 0)
+        BOTTOM_MIDDLE = (WIDTH/2, HEIGHT)
+
+        MIDDLE_LEFT = (0, HEIGHT/2)
+        MIDDLE_RIGHT = (WIDTH, HEIGHT/2)
+
+
+
+        self.X_Axis = (MIDDLE_LEFT, MIDDLE_RIGHT)
+        self.Y_Axis = (TOP_MIDDLE,BOTTOM_MIDDLE)
+        self.Increments = self.Increments
+
+
---
+++ main.py
@@ -0,0 +1,55 @@
+import pygame
+import cartisonGraph
+
+
+pygame.init()
+
+
+
+
+screen = pygame.display.set_mode((700,700), pygame.RESIZABLE)
+pygame.display.set_caption('Calculus demonstration')
+
+
+gameover = False
+clock = pygame.time.Clock()
+
+
+cordPlane = cartisonGraph.CartisonCords()
+
+
+
+
+
+
+while not gameover:
+    ticks = clock.get_time()
+    clock.tick(60)  # FPS
+    gameEvents = pygame.event.get()
+    # Input Section------------------------------------------------------------
+    for event in gameEvents:  # quit game if x is pressed in top corner
+        if event.type == pygame.QUIT:
+            gameover = True
+
+        if event.type == pygame.MOUSEWHEEL:
+            if cordPlane.Increments >= 1:
+                cordPlane.Increments += event.y
+            else:
+                cordPlane.Increments = cordPlane.Increments = 1
+
+            
+    #keyboard input-----------------------------------
+
+
+    cordPlane.update()
+    #render section-----------------------------------vis
+    screen.fill((150,150,150))
+
+    cordPlane.drawCartisonCords(screen) 
+
+
+
+    pygame.display.flip() #update graphics each game loop
+
+#END GAME LOOP#######################################################
+pygame.quit()
